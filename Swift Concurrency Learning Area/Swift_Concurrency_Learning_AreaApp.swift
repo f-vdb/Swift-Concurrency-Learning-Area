b884dda7989d7e934e908708879e9442e9cb6f71
fvdb <frank@am-knie.de>
2023-05-06 12:18:54 +0200

    Initial Commit

---
+++ Swift Concurrency Learning Area/Swift_Concurrency_Learning_AreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Concurrency_Learning_AreaApp.swift
+//  Swift Concurrency Learning Area
+//
+//  Created by Frank on 06.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Concurrency_Learning_AreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
